188a4db345d9c54d6c0109d87132b105535e07c0
SOurav <sourav_c@pixoatic.com>
2023-01-12 12:47:19 +0530

    authentication with google fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
+         
   after_save :welcome_email
 
   def self.from_omniauth(auth)
